e9cb5978097c87a6eb8006c9f1dce53445c11596
IEXPECTATIOM <1120644453@qq.com>
2021-08-06 12:01:50 +0800

    update -- 2021-08-06

---
+++ Build/main.js
@@ -0,0 +1,9 @@
+const srcPath = "../"
+const filePath = {
+  contentPath: srcPath + "Content/",
+  HackPath: srcPath + "Hack/",
+  happinessPath: srcPath + "Happiness/",
+  knowledgePath: srcPath + "Knowledge/",
+  MindPath: srcPath + "Mind/",
+  mirrorPath: srcPath + "Mirror/",
+}
