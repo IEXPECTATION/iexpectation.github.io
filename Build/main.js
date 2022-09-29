8fd77f567d428fda162ecbd986a83754528dacda
IEXPECTATION <1120644453@qq.com>
2022-09-29 09:10:04 +0800

    updates folder name

--- Build/main.js
+++
@@ -1,9 +0,0 @@
-const srcPath = "../"
-const filePath = {
-  contentPath: srcPath + "Content/",
-  HackPath: srcPath + "Hack/",
-  happinessPath: srcPath + "Happiness/",
-  knowledgePath: srcPath + "Knowledge/",
-  MindPath: srcPath + "Mind/",
-  mirrorPath: srcPath + "Mirror/",
-}
